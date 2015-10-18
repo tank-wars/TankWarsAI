53cb91c1eb748a6d7467c3a9157348a0d902fa51
Randixs <dulaj.r.atapattu@gmail.com>
2015-10-18 10:00:46 +0530

    GlpobalUpdate.cs added

---
+++ GameClient/GlobalUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClient
+{
+    class GlobalUpdate
+    {
+        public PlayerDetails[] PlayerUpdates { get; set; }
+
+        public Brick[] brickUpdate { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach(PlayerDetails player in PlayerUpdates)
+            {
+                builder.AppendLine(" ");
+                builder.AppendLine(player.ToString());
+            }
+
+            foreach(Brick brick in brickUpdate)
+            {
+                builder.AppendLine(brick.postition.ToString() + "\tDamage level: " + brick.damageLevel);
+            }
+            return builder.ToString();
+        }
+    }
+}
